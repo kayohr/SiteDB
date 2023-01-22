c0495bca6b70892d7e65dc94dc9fc489f2d5c3bc
kayo <kayohramos@gmail.com>
2023-01-22 20:17:27 -0300

    inicio

---
+++ my-app-redux/src/App.js
@@ -0,0 +1,14 @@
+import { Switch } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Switch>
+      {/* <Route exact path="/" component={  } />
+      <Route path="/carteira" component={  } /> */}
+
+    </Switch>
+  );
+}
+
+export default App;
---
+++ my-app-redux/src/redux/store.js
@@ -0,0 +1,17 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
